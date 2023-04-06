310eb1f2de6ed8aae9e85cb74611188d0a92429e
TheBinaryGuy <hello@thebinary.dev>
2023-04-06 11:34:49 +0530

    removed next-sitemap

--- next-sitemap.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl:
-        `https://${process.env.NEXT_PUBLIC_DOMAIN}` || 'https://example.com',
-    generateRobotsTxt: true,
-};
